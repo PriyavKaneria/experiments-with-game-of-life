ad98c32a1f92af5799a855523d0ac137fdc9dfe3
Priyav Kaneria <priyavkaneria@gmail.com>
2024-08-18 15:56:58 +0530

    Made a index page with router setup

---
+++ src/home.tsx
@@ -0,0 +1,42 @@
+const Home: React.FC = () => {
+	return (
+		<div className='p-4 font-serif text-gray-100 bg-black w-screen h-screen'>
+			<h1 className='text-6xl font-bold'>
+				<img
+					src='/logo.jpeg'
+					alt='logo'
+					className='h-16 w-16 inline-block mr-4 mb-3'
+				/>
+				Index
+			</h1>
+			<ul className='pl-5 list-disc list-outside [&_ul]:list-[revert]'>
+				<li className='mb-6 mt-4'>
+					<p className='text-5xl font-semibold'>2D</p>
+					<ul className='pl-5 text-2xl'>
+						<li className='my-3'>
+							<a href='/conwaysgol2D'>Conway's Game of Life 2D</a>
+						</li>
+						<li className='my-3'>
+							<a href='/conwaysgol2D-wasm'>
+								Conway's Game of Life 2D [Using Rust + WASM (WebAssembly)]
+							</a>
+						</li>
+					</ul>
+				</li>
+				<li className='my-6'>
+					<p className='text-5xl font-semibold'>3D</p>
+					<ul className='pl-5 text-2xl'>
+						<li className='my-3'>
+							<a href='/conwaysgol3D'>Conway's Game of Life 3D</a>
+						</li>
+						<li className='my-3'>
+							<a href='/qcautomata'>Quantum Computing Automata</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Home
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import { hydrate } from "react"
+import Router from "preact-router"
+import "./style.css"
 import QCAutomata from "./qcautomata"
-import ConwaysGOL from "./conwaysgol"
+import ConwaysGOL3D from "./conwaysgol3D"
+import ConwaysGOL2D from "./conwaysgol2D"
+import ConwaysGOL2DWASM from "./conwaysgol2D-wasm"
+import Home from "./home"
+
+const App = () => {
+	return (
+		<Router>
+			<Home path='/' />
+			<ConwaysGOL2D path='/conwaysgol2D' />
+			<ConwaysGOL2DWASM path='/conwaysgol2D-wasm' />
+			<ConwaysGOL3D path='/conwaysgol3D' />
+			<QCAutomata path='/qcautomata' />
+		</Router>
+	)
+}
 
 if (typeof window !== "undefined") {
-	hydrate(<ConwaysGOL />, document.getElementById("app"))
+	hydrate(<App />, document.getElementById("app"))
 }
