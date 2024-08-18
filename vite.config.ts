3b4bb5c682f7ba02af7cb6eca56017923f2b38a5
Priyav Kaneria <priyavkaneria@gmail.com>
2024-08-18 15:57:26 +0530

    Made logo and better homepage UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite"
+import preact from "@preact/preset-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +7,13 @@ export default defineConfig({
 		preact({
 			prerender: {
 				enabled: true,
-				renderTarget: '#app',
+				renderTarget: "#app",
 			},
 		}),
 	],
-});
+	server: {
+		fs: {
+			allow: [".."],
+		},
+	},
+})
